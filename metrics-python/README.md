de2bbf99d3b3eb216b267a3e34fe3d2e048e65a3
jtaylorcpp@gmail.com <jtaylorcpp@gmail.com>
2017-10-29 15:23:29 -0400

    Added alert function and printout

--- metrics-python/README.md
+++ metrics-python/README.md
@@ -16,33 +16,13 @@ Design and develop a metrics/monitoring system that is capable of ingesting a st
 ## Thoughts on Data
 
   * The smallest unit of time is 1 second
+
   * Multiple records come in every second
 
-### Possible Representations
+    * Two step analysis may be best, aggregate then infer
 
-* Heatmap - diff between current and prev build is color
 
-  * alert on time above certain threshold
-
-* Linegraph - rates of each code comparing current and prev build
-
-  * prolonged time above threshold triggers alert
-
-## Metrics
-
-  * Current thoughts on metrics consist of:
-    * counter
-    * gauge
-    * histogram
-
-## The Maths
-
-  * Rate/Velocity/Slope  -> x2 - x1/ t2 - t1
-    * amount of increase from last point
-  * Acceleration -> v2 - v1/ t2 t1
-    * how quickly the rate of the data is changing
-
-## Data Format
+### Data Format
 
 There are two sets of data, Set A and Set B. Both have three columns:
 
@@ -50,9 +30,9 @@ There are two sets of data, Set A and Set B. Both have three columns:
 Timestamp   Build_Version   Error_Code
 ```
 
-## Data Sets
+#### Data Sets
 
-## Data Set A
+##### Data Set A
 
 ```
 Length of Data Set A: 14000
@@ -66,7 +46,7 @@ Error_Code                  -17
 dtype: object
 ```
 
-## Data Set B
+##### Data Set B
 
 ```
 Length of Data Set B: 14000
@@ -80,5 +60,131 @@ Error_Code                   -3
 dtype: object
 ```
 
-## Thoughts on Streaming Metrics
+## Metrics Analysis Process
+
+After looking at the data, there are a few solution that jump out as being possible ways forward.
+
+Many monitoring/metrics platforms take advantage of data being in time-series and then utilize things like 
+counters, guages, histograms, and summaries to look and and infer behavior in the data.
+
+With this in mind, it made sense to use the test data files, which have timestamps, to build a solution based around time-series metrics. However, the data has multiple entries for each second but a second is the smallest time bin we can make for the date as the timestamps do not have microseconds or smallers.
+
+With seconds being the smallest bins and records coming into the system at a rate faster than 1 per second; the overall solution will be two parts: _Aggregation_ and _Analysis_
+
+### Aggregation
+
+The aggregation step in the system will take all records in a one second time frame and rreformat them in a way that is a bit more flexible. 
+
+It is assumed that there are 2 pieces of information known a priori:
+
+  * build versions
+
+  * error codes
+
+The aggregator then takes records in format:
+
+```
+Timestamp Build_Version Error_Code
+```
+
+And reformats them into:
+
+```
+Timestamp Build_Version1_Error_Code_1 Build_Version1_Error_Code_2 ... Build_Version2_Error_Code_1 ...
+```
+
+This allows the aggregator to take disparate and varied error records and turn them into a single record per second with each column being a counter.
+
+The final record looks something like:
+
+```
+["0:00:00" 0 0 0 0 1 2 3 0 0 0 .... 0 2 0 0]
+```
+
+### Analysis
+
+The analysis portion of the system now has access to records in which each error code is represented by a per build version counter.
+
+This allows for all sorts of different time-series algorithms to be used to accomplish different types of alerts or situational awareness.
+
+For this example, due to the visualization requirements, a heatmap seemed to be a logical way to move forward. Scatter plots or histograms could have been used but in order to make a 'read-time' visualization, the scatter plot would either be busy (up to 72 independent lines in one case) or overwhelming (18 different plots). A histogram would have also worked but watching for changes over time on a single histogram does not necessarily provide visuals for the historic data.
+
+A heatmap, on the other hand, only has to have an entry for unique error codes as we can compare build versions and show their relation through color. This allows for each segment of the visualization to convey the relationship between builds and error codes quickly on a time-series axis while also allowing for analysis based on time buckets and not instantaneous time stamps.
+
+#### Alerting
+
+Since the basis for alerts in the analysis are over a time series, whcih values are alerted on are also a function of the size of the bin that has been configured.
+
+If there is a 10000000% increase in errors in a 1 second bin, but the next 10 bins have 0 errors, is that something to worry about?
+
+THe somewhat simple approach taken is to evaluate the mean of a series of bins. In this way, alerts with become active and then become unactive based on the amount of time passed the the stream of errors. Sustained alerts over multiple time series for the same error code would then point to a problem.
+
+For this implementation, alerts looks like:
+
+```
+Alert: Mean Threshold Exceeded
+    Start Timestamp: 0:59:00
+    Stop Timstamp:   0:59:40
+    Error Code:      Error_Code_3
+    Value:           1.9000000000000004
+    Threshold:       1.25
+```
+
+Where, in this example, the threshold is set for a 25% increase in errors (1.25) and averaged over 5 time bins where each bin is 10 seconds. 
+
+## To Run
+
+This example is all written in Python 3.
+
+### Install Python 3 
+
+#### MAC OSX
+
+Please download and install from https://www.python.org/downloads/mac-osx/
+
+#### Windows
+
+Please download and install from https://www.python.org/downloads/windows/
+
+#### Linux
+
+Please follow this example for Ubuntu http://docs.python-guide.org/en/latest/starting/install3/linux/.
+
+For CentOS or other distros...sorry.
+
+### Install Pip for Python 3
+
+Pip is currently shipped with Ptyhon 3.4+. So no need to install.
+
+### Install Pipenv
+
+Unsing the terminal or terminal emualtor (Windows is Git Bash), run:
+
+```
+pip3 install pipenv
+```
+
+If there is a permission error, sudo can be used:
+
+```
+sudo pip3 install pipenv
+```
+
+Pipenv is a tool that segregates Python environments in a way that allows for multiple separate environments ot exist on one machine.
+
+### Install Project Dependencies
+
+With pipenv installed, we can now install all of the various dependencies.
+
+Navigate to this repo and run:
+
+```
+pipenv install
+```
+
+This will look into the Pipfile and Pipfile.lock and install the necessary dependencies in an environment to run the exmaple code.
+
+### Run examples
+
+By defualt, the code is configured to run against the 
 
--- metrics-python/data_model/data.py
+++ metrics-python/data_model/data.py
@@ -39,57 +39,77 @@ def analyze_pandas(df):
 def aggregate_to_time_series(df,build_alpha,build_beta,_size,_min,_max):
     # aggregator
     # start building composite data set better for a 'stream' analysis
+    # makes data 'double wide'
+    # timestamp , build_a_error_1, ...error_2, ..., build_b_error_1, ...error_2, ...
     alpha_columns = ["{1}_Error_Code_{0}".format(i,build_alpha) for i in range(_min,_max+1)] 
-    print(alpha_columns)
 
     beta_columns = ["{1}_Error_Code_{0}".format(i,build_beta) for i in range(_min,_max+1)]
-    #print(beta_columns)
+
+    indices = {'timestamp':0,build_alpha:1,build_beta:1+len(alpha_columns)}
+
+    '''
+    print(indexes)
+    print(alpha_columns)
+    print(beta_columns)
+    '''
 
     # take data, accumulate per second, make table double wide so index is timestamp
     data_stream = pandas.DataFrame(columns=['timestamp']+alpha_columns+beta_columns)
-    #print(data_stream)
-    indices = {'timestamp':0,build_alpha:1,build_beta:(2 + _max + (0-_min))}
-    #print(indexes)
-
-    #print(len(data_a.index))
-    #print(len(data_stream.columns.values),2 * (a_max - a_min + 1))
-    #print(data_a.loc[0]['Timestamp'])
+    
     current_timestamp = df.loc[0]['Timestamp']
     current_array = numpy.zeros(2 * (a_max - a_min + 1))
-    for df_index in range(_size):
-        if df.loc[df_index]['Timestamp'] != current_timestamp:
-            #print(len(data_stream.columns.values))
-            #print(len([current_timestamp]+current_array.tolist()))
-            data_stream.loc[len(data_stream)] = [current_timestamp]+current_array.tolist()
-            #print(data_stream)
-            # re-initialize numpy array
-            current_array = numpy.zeros(2 * (a_max - a_min + 1))
-            # reset timestamp
-            if df_index < _size - 1:
-                current_timestamp = data_a.loc[df_index + 1]['Timestamp']
-            # add first value
 
+    for df_index in range(_size):
+        if df.loc[df_index]['Timestamp'] != current_timestamp: 
+            data_stream.loc[len(data_stream)] = [current_timestamp]+current_array.tolist()   
+            current_array = numpy.zeros(2 * (a_max - a_min + 1))
+            # reset timestamp only if there is data for bucket
+            # i.e. dont build bucket for timestamp 1:00:01 
+            #   if there is no data to put in it
+            if df_index < _size - 1:
+                current_timestamp = df.loc[df_index]['Timestamp']
+            ''' for debug 
             if current_timestamp == "0:00:03":
-                print(data_stream)
+                #print(data_stream)
                 return data_stream
+            '''
         
         # still on same timestamp, add to array
-        print("Time: {2}, Build: {0}, Error Code: {1}".format(data_a.loc[df_index]['Build_Version'],
-                                                    data_a.loc[df_index]['Error_Code'],
-                                                    data_a.loc[df_index]['Timestamp']))
+        try:
+            current_index = indices[df.loc[df_index]['Build_Version']] + \
+                (df.loc[df_index]['Error_Code'] - _min)
 
-        current_index = indices[df.loc[df_index]['Build_Version']] + \
-            (df.loc[df_index]['Error_Code'] - _min)
+            current_array[current_index - 1] += 1
+        except Exception as e:
+            # debug info for aggregation problems
+            print(e)
+            print("Time: {2}, Build: {0}, Error Code: {1}".format(df.loc[df_index]['Build_Version'],
+                                                        df.loc[df_index]['Error_Code'],
+                                                        df.loc[df_index]['Timestamp']))
+            print(indices)
+            print(current_index)
+            print(len(current_array))
+            print(indices[df.loc[df_index]['Build_Version']])
+            print((df.loc[df_index]['Error_Code']))
+            print(0 - (_min+1))
+            
+    return data_stream
+'''
+Heatmap
 
-        current_array[current_index] += 1
+designed to manage timestamps in 'buckets' of size in seconds
 
-'''class Heatmap(object):
-    def __init__(self, bin_size=5,current_timestamp="0:00:00"):
+allows child classes to implement plotting and ingest patterns
+'''
+class Heatmap(object):
+    def __init__(self, bin_size=5,current_timestamp="0:00:00",threshold=1.25,threshold_bins = [1]):
         self.bin_size = bin_size
         self.current_timestamp = current_timestamp
         self.current_timestamp_int = self.timestamp_to_int(self.current_timestamp)
         self.next_timestamp_int = 0
         self.generate_next_timestamp()
+        self.threshold = threshold
+        self.threshold_bins = sorted(threshold_bins)
 
     def timestamp_to_int(self,timestamp):
         return (60*60*int(timestamp[0])) + (60*int(timestamp[2:4])) + int(timestamp[5:])
@@ -112,28 +132,106 @@ def aggregate_to_time_series(df,build_alpha,build_beta,_size,_min,_max):
     def plot(self):
         pass
 
-class DifferentialHeatmap(Heatmap):
-    def __init__(self,df_columns=[],bin_size=5,current_timestamp="0:00:00"):
-        super(DifferentialHeatmap, self).__init__(bin_size, current_timestamp)
-        columns_to_use = df_columns[:int(len(df_columns)/2)+1]
+    def assess_thresholds(self):
+        pass
+
+'''
+PercentialHeatmap
+
+Designed to find percent increase in error codes
+    i.e. build_a has 10 errors of type 5
+            build_b has 100 errors of type 5
+            100/10 is a tenfold increase or (1000%), which is bad
+
+    i.e. build_a has 10 errors of type 4
+            build_b has 9 errors of type 4
+            9/10 is a 10 percents decrease (90% of original)
+            may not be desired, but not a new 'feature'
+'''
+
+ALERT_STR = """
+Alert: Mean Threshold Exceeded
+    Start Timestamp: {0}
+    Stop Timstamp:   {1}
+    Error Code:      {2}
+    Value:           {4}
+    Threshold:       {3}
+    Bin Size:        {5}
+"""
+
+class PercentialHeatmap(Heatmap):
+    def __init__(self,df_columns=[],bin_size=5,current_timestamp="0:00:00",threshold=1.25,threshold_bins=[1]):
+        super(PercentialHeatmap, self).__init__(bin_size, current_timestamp,threshold,threshold_bins)
+        columns_to_use = df_columns[:int(len(df_columns)/2)]
         columns_to_build = ['_'.join(code.split('_')[1:]) for code in columns_to_use]
-        print(columns_to_build)
+        #print(columns_to_build)
         self.df = pandas.DataFrame(columns=['Timestamp']+columns_to_build)
 
     def ingest_record(self,record):
-        # record is in format
-        # timestamp, build_a_error_1, 2, 3 ...
+        # make pandas dataframe row in numpy matrix
         np_record = record.as_matrix()
-        new_row  = numpy.zeros(len(self.df))
-        print(len(np_record),len(new_row))
-        for index in range(1,len(np_record)):
-            if index <= len(new_row):
-                new_row[index-1] = -1 * np_record[index]
+
+        ## if the heatmap df is still unititialized, add first row
+        if len(self.df) == 0:
+            self.df.loc[0] = [self.int_timestamp_to_str(self.current_timestamp_int)] + numpy.zeros(len(self.df.columns.values)-1).tolist()
+        
+        # 1. check to make sure record is within time
+        if self.timestamp_to_int(np_record[0]) >= self.next_timestamp_int:
+            # 1. before adding new row or timestamps, assess alerts
+            self.assess_thresholds()
+            # 2. update timestamps
+            self.current_timestamp_int = self.next_timestamp_int
+            self.generate_next_timestamp()
+            # 3. add new row to df
+            self.df.loc[len(self.df)] = [self.int_timestamp_to_str(self.current_timestamp_int)] + numpy.zeros(len(self.df.columns.values)-1).tolist()
+        
+        # 2. update current row
+        #   since all records coming in are aggregations of timestamp
+        #   sum average percentages (1/bin_size) * new_error_count / old_error_count
+
+        row_size = len(self.df.loc[len(self.df) - 1]) - 1
+        for idx in range(row_size):
+            # if denominator is 0
+            if np_record[idx + 1] == 0 or np_record[idx + 1] == 0.0:
+                # if numerator is 0
+                if np_record[idx + row_size + 1] == 0 or np_record[idx + row_size + 1] == 0.0:
+                    # numerator and denominator are both 0 ~ call this "1"
+                    self.df.loc[len(self.df)-1,self.df.columns.values[idx+1]] += (1/self.bin_size)
+                else:
+                    # numerator is not 0 but denominator is which is undefined
+                    # numpy.nextafter(x,y) gives the smallest float number increment from x -> y
+                    # https://stackoverflow.com/questions/6063755/increment-a-python-floating-point-value-by-the-smallest-possible-amount
+                    # yet this results in inf which is not very helpful
+                    #self.df.loc[len(self.df)-1,self.df.columns.values[idx+1]] += (1/self.bin_size) * (np_record[idx + row_size + 1]/numpy.nextafter(0,1))
+                    # by comparison, adding 1 is very large as 1 ~= size of bin increase
+                    #   if bin is 600 wide, then adding 1 is like having 600 error count
+                    self.df.loc[len(self.df)-1,self.df.columns.values[idx+1]] += 0.05 * self.bin_size
             else:
-                new_row[index - 1 - len(np_record)] += np_record[index]
-        print(np_record)
-        print(new_row)
-'''
+                # both numerator and denominator are defined
+                self.df.loc[len(self.df)-1,self.df.columns.values[idx+1]] += (1/self.bin_size) * (np_record[idx + row_size + 1] / np_record[idx + 1])
+
+    def assess_thresholds(self):
+        currents_thresh_bins  = self.threshold_bins
+        # remove time bins to analyze f they dont exist
+        while (currents_thresh_bins[-1] > len(self.df)):
+            currents_thresh_bins = currents_thresh_bins[:-1]
+            if len(currents_thresh_bins) == 0:
+                break
+        
+        if len(currents_thresh_bins) > 0:
+            # for each bin length, check each column  for exceeding threshold
+            for column in self.df.columns.values[1:]:
+                # only look at error columns
+                for bin in currents_thresh_bins:
+                    # assess bin number of bins in column
+                    if self.df.loc[len(self.df)-bin:len(self.df)-1,column].mean() > self.threshold:
+                        print(ALERT_STR.format(self.df.loc[len(self.df)-bin,"Timestamp"],self.df.loc[len(self.df)-1,'Timestamp'],
+                                                column,self.threshold,self.df.loc[len(self.df)-bin:len(self.df)-1,column].mean(),
+                                                bin))
+
+
+        
+
         
 
 
@@ -141,6 +239,10 @@ class DifferentialHeatmap(Heatmap):
 
 if __name__ == '__main__':
     build_alpha, build_beta = '5.0007.510.011','5.0007.610.011'
+    '''
+    TEST on ErrorStreamSetA.csv
+    '''
+    #'''
     data_a = load_data_pandas('data_model/ErrorStreamSetA.csv','\t')
     
     # find len of data set and assess cardinality
@@ -148,11 +250,17 @@ if __name__ == '__main__':
 
     data_stream = aggregate_to_time_series(data_a,build_alpha,build_beta,a_len,a_min,a_max)
     
-    '''print('heatmap test')
-    heatmap_5s = DifferentialHeatmap(df_columns=data_stream.columns.values[1:],bin_size=600)
-    print(heatmap_5s.bin_size)
-    print(heatmap_5s.current_timestamp)
-    print(heatmap_5s.current_timestamp_int,heatmap_5s.int_timestamp_to_str(heatmap_5s.current_timestamp_int))
-    print(heatmap_5s.next_timestamp_int,heatmap_5s.int_timestamp_to_str(heatmap_5s.next_timestamp_int))
-    print(data_stream.loc[0])
-    heatmap_5s.ingest_record(data_stream.loc[0])'''
+    print('heatmap test')
+    heatmap_10m = PercentialHeatmap(df_columns=data_stream.columns.values[1:],bin_size=10,threshold_bins=[1,5,10])
+    
+    # 'streaming' of data
+    for idx in range(len(data_stream)):
+        heatmap_10m.ingest_record(data_stream.loc[idx])
+    print(heatmap_10m.df.T)
+    #sns.heatmap(heatmap_5s.df,annot=True)
+    #'''
+    '''
+    TEST on ErrorStreamSetB.csv
+    '''
+    '''
+    '''
